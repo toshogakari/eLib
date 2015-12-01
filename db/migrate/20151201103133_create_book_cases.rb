c3495594aba8480e7f4cf769bfab4c00b566487b
pyar6329 <pyar6329@gmail.com>
2015-12-02 01:07:44 +0900

    changed tinyint to integer

--- db/migrate/20151201103133_create_book_cases.rb
+++ db/migrate/20151201103133_create_book_cases.rb
@@ -1,8 +1,8 @@
 class CreateBookCases < ActiveRecord::Migration
   def change
     create_table :book_cases do |t|
-      t.column :evaluation, :tinyint, null: false, default: 0
-      t.column :level, :tinyint, null: false, default: 0
+      t.integer :evaluation, null: false, default: 0
+      t.integer :level, null: false, default: 0
       t.references :user, index: true, foreign_key: true
       t.references :book, index: true, foreign_key: true
 
