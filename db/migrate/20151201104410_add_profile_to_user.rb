55cedc884947eff2f3c7d0a124750035dceb07dc
pyar6329 <pyar6329@gmail.com>
2015-12-02 01:13:15 +0900

    changed tinyint to integer

--- db/migrate/20151201104410_add_profile_to_user.rb
+++ db/migrate/20151201104410_add_profile_to_user.rb
@@ -2,7 +2,7 @@ class AddProfileToUser < ActiveRecord::Migration
   def change
     change_table :users do |t|
       t.string :name, null: false, default: ''
-      t.column :experience, :tinyint, null: false, default: 0
+      t.integer :experience, null: false, default: 0
       t.text :profile
       t.references :job, index: true, foreign_key: true
     end
