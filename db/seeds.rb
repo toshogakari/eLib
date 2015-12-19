551b5aabe7e27764d0ba54148d4f273fe8ce3cca
noguhiro <g007c1280@gmail.com>
2015-12-19 20:27:11 +0900

    add book seed, category seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+####################################################################
+# Categories
+####################################################################
+categories = %w(プログラミング ネットワーク セキュリティ Webアプリケーション モバイルアプリケーション データベース データ解析 Apple)
+
+categories.each do |category|
+  Category.create(name: category)
+end
+
+####################################################################
+# Books
+####################################################################
+books = ActiveSupport::JSON.decode(File.read('db/seeds/books.json'))
+random = Random.new
+
+books.each do |book|
+  Book.create(
+      isbn: book['isbn'],
+      title: book['title'],
+      description: book['description'],
+    pc_image_url: book['pc_image_url'],
+    mb_image_url: book['mb_image_url'],
+    category_id: random.rand(1 ... categories.length).to_i
+  )
+end
