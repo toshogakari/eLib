3a498becd8407fa4c1726501a4076dbc980443cf
noguhiro <g007c1280@gmail.com>
2015-12-22 00:04:26 +0900

    add package react.

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,8 @@ module ELib
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # seting br
+    config.browserify_rails.commandline_options = '-t babelify'
   end
 end
