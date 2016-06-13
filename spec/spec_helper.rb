12f959de064e40efe2a367d2fd76e37d2e9e6363
Tsukasa Arima <pyar6329@gmail.com>
2016-06-13 23:12:57 +0900

    change test coverage from codeclimate to coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,8 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-require 'codeclimate-test-reporter'
+require 'coveralls'
+Coveralls.wear!('rails') if ENV['COVERALLS_TOKEN'].present?
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
@@ -92,5 +93,5 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
   config.color = true
-  CodeClimate::TestReporter.start unless ENV['CODECLIMATE_REPO_TOKEN'].nil?
+
 end
